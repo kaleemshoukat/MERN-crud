c2fef5c223e39a69bdcb286b6731b3bda8d47bcb
kaleem <ks@softwarealliance.dk>
2022-07-01 18:24:55 +0500

    model add user completed

--- src/Components/User/AddUser.jsx
+++ src/Components/User/AddUser.jsx
@@ -57,12 +57,12 @@ const AddUser=()=>{
             stripUnknown: true // remove unknown props
         }
 
-        const validation = schema.validate(formData, options)
-        if(validation.error){
-            console.log(validation.error)
-            //toast(validation.error)
-        }
-        else {
+        // const validation = schema.validate(formData, options)
+        // if(validation.error){
+        //     console.log(validation.error)
+        //     //toast(validation.error)
+        // }
+        // else {
             const result=await axios.post('http://localhost:3001/users/add', formData);
             const response=result.data
 
@@ -74,7 +74,7 @@ const AddUser=()=>{
             else{
                 toast(response.message)
             }
-        }
+        // }
     };
 
     return(
@@ -85,8 +85,8 @@ const AddUser=()=>{
                         <h5 className="modal-title" id="exampleModalLabel">Add User</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div className="modal-body">
-                        <form className="create-form" encType="multipart/form-data">
+                    <form className="create-form" encType="multipart/form-data">
+                        <div className="modal-body">
                             <div className="col-md-12">
                                 <div className="row">
                                     <div className="col-md-6">
@@ -126,12 +126,12 @@ const AddUser=()=>{
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" onClick={handleSubmit} className="btn btn-primary">Submit</button>
-                    </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" onClick={handleSubmit} className="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -3,12 +3,12 @@ import axios from "axios";
 import ReactPaginate from "react-paginate";
 import {confirmAlert} from "react-confirm-alert";
 import {toast} from "react-toastify";
-import {Link} from "react-router-dom";
 import Loader from "./Loader";
-import AddUser from "./User/AddUser";
+import {Button, Modal, ModalBody, ModalHeader, ModalTitle, ModalFooter} from "react-bootstrap";
+const Joi = require('joi');
 
 const Users=()=>{
-    // We start with an empty list of items.
+    // pagination
     const itemsPerPage=10
     const [currentItems, setCurrentItems] = useState(null);
     const [itemsCount, setItemsCount] = useState(0);
@@ -41,6 +41,7 @@ const Users=()=>{
 
     },  [itemOffset, itemsPerPage]);
 
+    //delete row
     const deleteSubmit=(id)=>{
         //alert(id);
         confirmAlert({
@@ -74,13 +75,177 @@ const Users=()=>{
         });
     }
 
+    //add model
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {
+        setShow(false);
+        setErrors({});
+    };
+    const handleShow = () => {
+        setShow(true);
+    }
+
+    //add form submit
+    const [errors, setErrors] = useState({});
+    const [Values, setValues] = useState({});
+    const [image, setImage] = useState();
+
+    const changeHandler = (e) => {
+        setValues({ ...Values, [e.target.name]: e.target.value });
+    };
+    const FileHandler = (e) => {
+        // let files = files;
+        setImage(e.target.files[0]);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var data = new FormData(event.target)
+        let formData = Object.fromEntries(data.entries())
+        console.log(formData)
+
+        const schema = Joi.object().keys({
+            name: Joi.string().max(255).required().empty('').messages({
+                'string.max': 'This field can have maximum length of {#limit}.',
+                'any.required': 'This field is required.'
+            }),
+            email: Joi.string().max(255).required().empty('').trim(true).messages({
+                'string.max': 'This field can have maximum length of {#limit}.',
+                'any.required': 'This field is required.',
+                'string.email': 'This field has invalid email.',
+            }),
+            gender: Joi.string().max(255).required().empty('').messages({
+                'string.max': 'This field can have maximum length of {#limit}.',
+                'any.required': 'This field is required.',
+            }),
+            profileImage: Joi.any().required().empty('').messages({
+                'any.required': 'This field is required.',
+            }),
+            country: Joi.string().max(255).required().empty('').messages({
+                'string.max': 'This field can have maximum length of {#limit}.',
+                'any.required': 'This field is required.',
+            }),
+            cgpa: Joi.number().integer().precision(1).max(4).required().empty('').messages({
+                'number.max': 'This field can have maximum length of {#limit}.',
+                'any.required': 'This field is required.',
+            }),
+            password: Joi.string().min(3).max(15).required().empty('').trim(true).messages({
+                'string.min': 'This field should have a minimum length of {#limit}.',
+                'string.max': 'This field can have maximum length of {#limit}.',
+                'any.required': 'This field is required.',
+            })
+        })
+
+        // schema options
+        const options = {
+            abortEarly: false, // include all errors
+            allowUnknown: true, // ignore unknown props
+            stripUnknown: true // remove unknown props
+        }
+
+        const validation = schema.validate(formData, options)
+        if(validation.error){
+            const items=validation.error.details
+            const errors = {};
+            for (let item of items) {
+                errors[item.path[0]] = item.message;
+                //toast(`${item.path[0]} ${item.message}`);
+            }
+            console.log(errors);
+            setErrors(errors);
+        }
+        else {
+            const result=await axios.post('http://localhost:3001/users/add', formData);
+            const response=result.data
+
+            if (response.status){
+                event.target.reset();
+                setShow(false);
+                toast(response.message);
+                paginate(itemOffset, itemsPerPage);
+            }
+            else{
+                toast(response.message)
+            }
+        }
+    }
+
     if(loading===false) return <Loader/>;
     return(
         <div className="container">
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addUserModal">
+            <Button variant="primary" onClick={handleShow}>
                 Add User
-            </button>
-            <AddUser />
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add User</Modal.Title>
+                </Modal.Header>
+                <form onSubmit={handleSubmit} className="create-form" encType="multipart/form-data">
+                    <Modal.Body>
+                        <div className="col-md-12">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <label>Name</label>
+                                    <input type="text" name="name" onChange={changeHandler} className="form-control" />
+                                    {errors.name && (<label className="error text-danger">{errors.name}</label>)}
+                                </div>
+                                <div className="col-md-6">
+                                    <label>Email</label>
+                                    <input type="text" name="email" onChange={changeHandler} className="form-control" />
+                                    {errors.email && (<label className="error text-danger">{errors.email}</label>)}
+                                </div>
+                                <div className="col-md-6">
+                                    <label>Password</label>
+                                    <input type="password" name="password" onChange={changeHandler} className="form-control" />
+                                    {errors.password && (<label className="error text-danger">{errors.password}</label>)}
+                                </div>
+                                <div className="col-md-6">
+                                    <label>Gender</label>
+                                    <div onChange={changeHandler}>
+                                        <input type="radio" name="gender" value="Male" /> Male
+                                        <input type="radio" name="gender" value="Female" /> Female
+                                    </div>
+                                    {errors.gender && (<label className="error text-danger">{errors.gender}</label>)}
+                                </div>
+                                <div className="col-md-6">
+                                    <label>Profile Image</label>
+                                    <input type="file" name="profileImage" onChange={FileHandler} className="form-control" />
+                                    {errors.profileImage && (<label className="error text-danger">{errors.profileImage}</label>)}
+                                </div>
+                                <div className="col-md-6">
+                                    <label>CGPA</label>
+                                    <input type="number" name="cgpa" onChange={changeHandler} className="form-control" />
+                                    {errors.cgpa && (<label className="error text-danger">{errors.cgpa}</label>)}
+                                </div>
+                                <div className="col-md-6">
+                                    <label>Country</label>
+                                    <select name="country" onChange={changeHandler} className="form-control">
+                                        <option value="">Select Option</option>
+                                        <option value="Pakistan">Pakistan</option>
+                                        <option value="India">India</option>
+                                    </select>
+                                    {errors.country && (<label className="error text-danger">{errors.country}</label>)}
+                                </div>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="submit">
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </Modal>
+
+
+
             <div className="table-responsive">
                 <table className="table">
                     <thead>
@@ -108,7 +273,9 @@ const Users=()=>{
                                 <td>{item.cgpa.$numberDecimal}</td>
                                 <td>{item.country}</td>
                                 <td>
-                                    <Link to={`/edit/${item._id}`} className="btn btn-primary btn-sm mr-2">Edit</Link>
+                                    <Button className="btn btn-secondary btn-sm" onClick={handleShow}>
+                                        Edit
+                                    </Button>
                                     <button onClick={() => deleteSubmit(item._id)} type="button" className="btn btn-danger btn-sm">
                                         Delete
                                     </button>
