d33910736da8489b23dd3ccc1d83bdfd3b96f624
kaleem <ks@softwarealliance.dk>
2022-08-02 12:35:58 +0500

    work on localization

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//redux
 import {Provider} from 'react-redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
